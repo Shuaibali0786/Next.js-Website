d6a44bbade3c66380cfbc363b39e9cb51834d5b9
Shuaibali0786 <160752334+Shuaibali0786@users.noreply.github.com>
2024-10-20 21:10:56 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="home">
+        <div className="right">
+            <h1>Welcome!</h1>
+            <h2>Your Website</h2>
+            <div className="button">
+                <button className="btn">Explore...</button>
+            </div>
+        </div>
+        <div className="left">
+            <form>
+                <h2>Login</h2>
+                <input type="text" className="shuaib" placeholder="User-Name" required/> <br/>
+                <input type="password" className="shuaib" placeholder="User-Pasworld"required/> <br/>
+                <div className="checkbox">
+                    <input className="check" type="checkbox"  /><h3>agree</h3>
+                    <a href="">Forget Pasworld</a>
+                </div>
+                <div className="button">
+                    <button className="btn">Login In</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+  
+  );
+}
---
+++ src/component/NavBar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import "../style/style.css"
+
+export default function NavBar() {
+    return (
+       <header>
+        <h1>Shuaib</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+
+            </ul>
+        </nav>
+        <div className="button">
+            <button className="btn">Sign Up</button>
+            <button className="btn">Log In</button>
+        </div>
+       </header>
+    );
+}
